d9ac2185d47743b456068e608cb01f25d2cd4599
Mason Lanham <masont.lanham@gmail.com>
2026-04-30 22:22:32 -0500

    Create FirstTrueInASortedBooleanArray.cs
    An array of boolean values is divided into two sections: The left section consists of all false, and the right section consists of all true. Find the First True in a Sorted Boolean Array of the right section, i.e., the index of the first true element. If there is no true element, return -1.
    
    Input: arr = [false, false, true, true, true]
    
    Output: 2
    
    Explanation: The first true's index is 2.
    
    Practicing C# for Unity development.

---
+++ Binary-Search/C#/FirstTrueInASortedBooleanArray.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    public static int FindBoundary(List<bool> arr)
+    {
+        int low = 0;
+        int high = arr.Count - 1;
+        int result = -1;
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid]){
+                result = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return result;
+    }

c3093b804dfcfa2933e6612a3036745d0462b5aa
Mason Lanham <masont.lanham@gmail.com>
2026-06-17 23:40:27 -0500

    Create FirstElementOfTargetInArrayWithDuplicates.py

---
+++ Binary-Search/Python/FirstElementOfTargetInArrayWithDuplicates.py
@@ -0,0 +1,32 @@
+'''
+Given a sorted array of integers and a target integer, find the first occurrence of the target and return its index. Return -1 if the target is not in the array.
+
+Input:
+
+arr = [1, 3, 3, 3, 3, 6, 10, 10, 10, 100]
+target = 3
+Output: 1
+
+Explanation: The first occurrence of 3 is at index 1.
+
+Input:
+
+arr = [2, 3, 5, 7, 11, 13, 17, 19]
+target = 6
+Output: -1
+
+Explanation: 6 does not exist in the array.
+'''
+def find_first_occurrence(arr: list[int], target: int) -> int:
+    result = -1
+    low = 0
+    high = len(arr) - 1
+    while(low <= high):
+        mid = low + (high - low) // 2
+        if(arr[mid] >= target):
+            high = mid - 1
+            if(arr[mid] == target):
+                result = mid
+        else:
+            low = mid + 1
+    return result

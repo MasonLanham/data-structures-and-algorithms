e5852942fa01a8b1d07c267bc7f071fae0b0d335
Mason Lanham <masont.lanham@gmail.com>
2026-06-28 21:35:19 -0500

    Create MiddleOfLinkedList.py

---
+++ TwoPointers/SameDirection/Python/MiddleOfLinkedList.py
@@ -0,0 +1,24 @@
+'''Find the middle node of a linked list.
+
+Input: 0 1 2 3 4
+
+Output: 2
+
+If the number of nodes is even, then return the second middle node.
+
+Input: 0 1 2 3 4 5
+
+Output: 3'''
+
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+def middle_of_linked_list(head: Node) -> int:
+    slow = head
+    fast = head
+    while(fast != None and fast.next != None):
+        slow = slow.next
+        fast = fast.next.next
+    return slow.val

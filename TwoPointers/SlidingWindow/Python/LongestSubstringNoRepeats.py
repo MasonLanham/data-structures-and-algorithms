20e6d13e6f9cd1ac18f75894ed33f1b4394380c0
Mason Lanham <masont.lanham@gmail.com>
2026-07-17 18:38:01 -0500

    Update LongestSubstringNoRepeats.py
    Simplified and easier to understand implementation.

--- TwoPointers/SlidingWindow/Python/LongestSubstringNoRepeats.py
+++ TwoPointers/SlidingWindow/Python/LongestSubstringNoRepeats.py
@@ -22,26 +22,15 @@ def longest_substring_without_repeating_characters(s: str) -> int:
     left = 0
     right = 0
     longest = 0
-    while(right < len(s)):
-        #expand window while valid
-        while(right < len(s) and map[ord(s[right]) - ord('1')] < 1):
-            map[ord(s[right]) - ord('1')] += 1
-            right += 1
-            
-        longest = max(longest, right - left)
-
-        #break if right is outside bounds
-        if right >= len(s):
-            break
-            
-        #shrink window until valid
-        while(left <= right and map[ord(s[right]) - ord('1')] > 0):
+    while right < len(s):
+        #Expand Window
+        map[ord(s[right]) - ord('1')] += 1
+        #Maintain Valid Window
+        while map[ord(s[right]) - ord('1')] > 1:
             map[ord(s[left]) - ord('1')] -= 1
             left += 1
-            
-            
+        #Update longest
+        longest = max(longest, right - left + 1)
+        right += 1
     return longest
-if __name__ == "__main__":
-    s = input()
-    res = longest_substring_without_repeating_characters(s)
-    print(res)
+
